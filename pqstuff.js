c8c896cabc0dfcac35dceef9ab00e7bfd170690e
K.V. <30656341+KVonGit@users.noreply.github.com>
2025-01-17 01:22:19 -0600

    Update phase 1
    - Remove old JS "fixes" for clearScreen and addText
    - Rename questjs to js
    - Fix 'save' command to use JS.saveGame() when online
    - Fix typo in either text processor command
    - Add 'Save' to the sidenave commands
    - Begin trying to make the UI reload correctly after loading a save

---
+++ pqstuff.js
@@ -0,0 +1,38 @@
+﻿var _animateScroll = false;
+
+function scrollToEndBak() {
+    var scrollTo = _animateScroll ? beginningOfCurrentTurnScrollPosition - 50 - $("#gamePanelSpacer").height() : $(document).height();
+    var currentScrollTop = Math.max($("body").scrollTop(), $("html").scrollTop());
+    if (scrollTo > currentScrollTop) {
+        var maxScrollTop = $(document).height() - $(window).height();
+        if (scrollTo > maxScrollTop) scrollTo = maxScrollTop;
+        var distance = scrollTo - currentScrollTop;
+        var duration =  distance / 0.4;
+        // Added by The Pixie on behalf of alexandretorres
+        if (duration>2000) duration=2000;
+        $("body,html").stop().animate({ scrollTop: scrollTo }, duration, "easeInOutCubic");
+    }
+    $("#txtCommand").focus();
+    // Added by The Pixie; this is a fall back, as the above seems not to work on some browsers
+    // In fact it may be the all the rest of this can deleted
+    $('html,body').animate({ scrollTop: document.body.scrollHeight }, 'fast');
+};
+
+var scrollToEnd = function() {
+    var scrollTo = _animateScroll ? beginningOfCurrentTurnScrollPosition - 50 - $("#gamePanelSpacer").height() : $(document).height();
+    var currentScrollTop = Math.max($("body").scrollTop(), $("html").scrollTop());
+    if (scrollTo > currentScrollTop) {
+        var maxScrollTop = $(document).height() - $(window).height();
+        if (scrollTo > maxScrollTop) scrollTo = maxScrollTop;
+        var distance = scrollTo - currentScrollTop;
+        var duration = _animateScroll ? distance / 0.4 : 1;
+        /* Added by The Pixie on behalf of alexandretorres*/
+        if (duration>2000) duration=2000;
+        $("body,html").stop().animate({ scrollTop: scrollTo }, duration, "easeInOutCubic");
+    }
+   $("#txtCommand").focus();
+};
+
+function setCommandBarStyle(style) {
+  /* DO NOTHING */
+}
